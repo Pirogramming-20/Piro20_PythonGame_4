7d160bf5e180846235ef9d9c1e07a788e9090880
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-01-07 23:43:43 +0900

    game4 수정

--- game4.py
+++ game4.py
@@ -1,32 +1,30 @@
 import random
-
-# #{select_player.name} DOWN DOWN DOWN!! 𖦹ࡇ𖦹 𖦹ࡇ𖦹 𖦹ࡇ𖦹
-# class Player:
-#     def __init__(self, name, limit):
-#         self.name = name
-#         self.limit = limit
-#         self.drinks_had = 0
-
-#     def drink(self):
-#         self.drinks_had += 1
-#         if self.drinks_had > self.limit:
-#             return True
-#         return False
-
+#  DOWN DOWN DOWN!! 𖦹ࡇ𖦹 𖦹ࡇ𖦹 𖦹ࡇ𖦹
 
 def game4(players, start_index, name):
     current_player = players[start_index]  # 시작 플레이어
     rejection_count = 0
+    print("술도 마셨는데 좋아 게임할까~ ")
 
     while True:
         if current_player == name:  # '나'일 때
-            print("다음 지목할 사람을 선택하세요:")
-            for i, p in enumerate(players[1:], 1):
+            print("누가 좋아?:")
+            for i, p in enumerate(players[1:], 1): #'나' 0번째 인덱스
                 print(f"{i}: {p.name}")
-            choice = int(input()) - 1
-            #숫자 아닐땐 예외처리
-            select_player = players[choice + 1]
-            print(f"{current_player.name} : {select_player.name} 좋아~")
+            
+            try:
+                choice= int(input())
+
+                if choice in i:
+                    select_player = players[choice]
+                    print(f"{current_player.name} : {select_player.name} 좋아~")
+
+                #범위 내 숫자 아닐땐 예외처리
+                else:
+                    print("제대로 입력하세요")
+            except ValueError:
+                    print("숫자를 입력하세요.")
+          
         else:
             select_player = random.choice([p for p in players if p != current_player])
             print(f"{current_player.name} : {select_player.name} 좋아~")
@@ -40,25 +38,9 @@ def game4(players, start_index, name):
         else:
             rejection_count += 1
             if rejection_count == 3:
-                print(f" ヘ（゜◇、゜）ノ  누가 술을 마셔 {select_player.name}가 술을 마셔.  {select_player.name} 한잔해~   ヘ（゜◇、゜）ノ")
+                print(f" ヘ（゜◇、゜）ノ  {select_player.name} 한잔해~   ヘ（゜◇、゜）ノ")
+                loser_index = players.index(select_player)
+                return loser_index
                 break
-
- 
-       
-
-    return #패자의 인덱스.
-
-# 플레이어 객체 생성
-#player_a = Player("a", 3) # 입력 값,  나머진 랜덤으로 받아오기
-#player_b = Player("b", 5)
-#player_c = Player("c", 2)
-#player_d = Player("d", 4)
-
-# 플레이어 객체들을 포함하는 리스트 생성
-#players = [player_a, player_b, player_c, player_d]
-
-# 게임 시작과 결과 
-#game_result = game4(players)
-#print("\n".join(game_result))
 
 
