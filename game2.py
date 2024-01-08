f850037270253cc62da2b1ccf20182af96d25a1a
jaeyeong13 <141209201+jaeyeong13@users.noreply.github.com>
2024-01-08 16:42:49 +0900

    Update game2.py

--- game2.py
+++ game2.py
@@ -2,20 +2,26 @@ import random
 
 #play_apartment_game
 def game2(players,start_idx):
-    leader = random.choice(players)
-    print(f"술래: {leader}")
+    leader = players[start_idx].name
+    number = 0
+    if (start_idx == 0):
+        number = input(f"몇 층?! : ")
+    else:
+        number = random.randint(1, 20)
+        print(f"몇 층?! : {number}")
 
-    hands = [f"{player}의 손" for player in players for _ in range(2)]
+    hands = [f"{player.name}의 손" for player in players for _ in range(2)]
 
     random.shuffle(hands)
-
-    number = random.randint(10, 99)
-    print(f"술래가 부른 숫자: {number}")
 
     for i in range(1, number + 1):
         current_hand = hands[(i - 1) % len(hands)]
         print(f"{i}층: {current_hand}")
         if i == number:
             loser = current_hand.split('의')[0]
-            loser_index = players.index(loser)
-            return loser_index
+            loser_index = 0
+            for player in players:
+                if (loser == player.name):
+                    return loser_index
+                else:
+                    loser_index += 1
