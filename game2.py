7c4e3f37e18c52ccfa10d73a6fcef9279e9cea78
jaeyeong13 <141209201+jaeyeong13@users.noreply.github.com>
2024-01-08 20:39:27 +0900

    Update game2.py

--- game2.py
+++ game2.py
@@ -1,16 +1,29 @@
 import random
 
-#play_apartment_game
 def game2(players,start_idx):
+    apartment_asciiart = '''
+                        _                        _   
+  __ _ _ __   __ _ _ __| |_ _ __ ___   ___ _ __ | |_ 
+ / _` | '_ \ / _` | '__| __| '_ ` _ \ / _ \ '_ \| __|
+| (_| | |_) | (_| | |  | |_| | | | | |  __/ | | | |_ 
+ \__,_| .__/ \__,_|_|   \__|_| |_| |_|\___|_| |_|\__|
+      |_|                                            
+    '''
+    print("--------------------------------------------------------")
+    print(apartment_asciiart)
+    print("--------------------------------------------------------")
+    print("🏢🏢🏢 아~파트 아파트 아~파트 아파트 🏢🏢🏢")
+    print("--------------------------------------------------------")
     leader = players[start_idx].name
     number = 0
+    print(f"술래는 {leader}!")
     if (start_idx == 0):
-        number = int(input(f"몇 층?! : "))
+        number = input(f"몇 층?! : ")
     else:
-        number = random.randint(1, 20)
+        number = random.randint(5, 30)
         print(f"몇 층?! : {number}")
 
-    hands = [f"{player.name}의 손" for player in players for _ in range(2)]
+    hands = [f"{player.name}" for player in players for _ in range(2)]
 
     random.shuffle(hands)
 
@@ -18,7 +31,7 @@ def game2(players,start_idx):
         current_hand = hands[(i - 1) % len(hands)]
         print(f"{i}층: {current_hand}")
         if i == number:
-            loser = current_hand.split('의')[0]
+            loser = current_hand
             loser_index = 0
             for player in players:
                 if (loser == player.name):
